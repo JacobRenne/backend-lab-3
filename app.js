145f0e864f8c5f18220c9a42d72357721b8733a4
Jacob Rennemark <jacobrane.se@gmail.com>
2025-04-25 18:59:07 +0200

    Add all remaining requirements

--- app.js
+++ app.js
@@ -37,9 +37,9 @@ app.get('/api/games/:id', async(req, res) => {
   
   try {
     const game = await getGame(id)
-    return res.json({game})
+    res.json(game[0])
   } catch (error) {
-    return res.status(500).json({ error: error.message })
+    res.status(500).json({ error: error.message })
   }
 })
 
@@ -56,7 +56,7 @@ app.post('/api/games', async(req, res) => {
 
   try {
     await createGame(title, developer, releaseYear, price, categoryId)
-    return res.status(201).json({ success: true, message: 'Game added' })
+    res.status(201).json({ success: true, message: 'Game added' })
   } catch (error) {
     res.status(500).json({ success: false, error: error.message })
   }
@@ -75,9 +75,9 @@ app.put('/api/games', async(req, res) => {
 
   try {
     await updateGame(title, developer, releaseYear, price, categoryId, gameId)
-    return res.status(201).json({ success: true, message: 'Game updated' })
+    res.status(201).json({ success: true, message: 'Game updated' })
   } catch (error) {
-    return res.status(500).json({ success: false, error: error.message })
+    res.status(500).json({ success: false, error: error.message })
   }
 })
 
@@ -93,11 +93,128 @@ app.delete('/api/games/:id', async(req, res) => {
 
   try {
     await deleteGame(id)
-    return res.status(201).json({ success: true, message: 'Game deleted' })
+    res.status(201).json({ success: true, message: 'Game deleted' })
   } catch (error) {
-    return res.status(500).json({ success: false, error: error.message })
+    res.status(500).json({ success: false, error: error.message })
+  }
+})
+// Statistics requirement
+async function getGamesCount() {
+  const [rows] = await db.execute('SELECT COUNT(*) AS gameCount FROM games')
+  return rows
+}
+
+app.get('/api/count', async(req, res) => {
+  try {
+    const gameCount = await getGamesCount()
+    res.json(gameCount[0])
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message })
   }
 })
 
 ///////// Categories table
+async function getCategories() {
+  const [rows] = await db.execute('SELECT * FROM categories')
+  return rows
+}
 
+app.get('/api/categories', async(req, res) => {
+  try {
+    const categories = await getCategories()
+    res.json(categories)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+async function getCategory(id) {
+  const [rows] = await db.execute('SELECT * FROM categories WHERE categoryId = ?', [id])
+  return rows
+}
+
+app.get('/api/categories/:id', async(req, res) => {
+  const { id } = req.params
+  
+  try {
+    const category = await getCategory(id)
+    res.json(category[0])
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+async function createCategory(categoryName) {
+  let sql = 'INSERT INTO categories (categoryName) VALUES (?)'
+  
+  const [result] = await db.execute(sql, [categoryName])
+  return result
+}
+
+app.post('/api/categories', async(req, res) => {
+  const { categoryName } = req.body
+
+  try {
+    await createCategory(categoryName)
+    res.status(201).json({ success: true, message: 'Category added' })
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message })
+  }
+})
+
+async function updateCategory(categoryName, categoryId) {
+  let sql = 'UPDATE categories SET categoryName = ? WHERE categoryId = ?'
+  let params = [categoryName, categoryId]
+
+  const [result] = await db.execute(sql, params)
+  return result
+} 
+
+app.put('/api/categories', async(req, res) => {
+  const { categoryName, categoryId } = req.body
+
+  try {
+    await updateCategory(categoryName, categoryId)
+    res.status(201).json({ success: true, message: 'Category updated' })
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message })
+  }
+})
+
+async function deleteCategory(id) {
+  let sql = 'DELETE FROM categories WHERE categoryId = ?'
+
+  const [result] = await db.execute(sql, [id])
+  return result
+}
+
+app.delete('/api/categories/:id', async(req, res) => {
+  const { id } = req.params
+
+  try {
+    await deleteCategory(id)
+    res.status(201).json({ success: true, message: 'Category deleted' })
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message })
+  }
+})
+
+// VG Join requirement
+async function getGamesAndCategories() {
+  const sql = `
+  SELECT games.title, games.developer, games.releaseYear, games.price, categories.categoryName
+  FROM games
+  JOIN categories ON games.categoryId = categories.categoryId`
+
+  const [rows] = await db.execute(sql)
+  return rows
+}
+
+app.get('/api/games-categories', async(req, res) => {
+  try {
+    const gamesAndCats = await getGamesAndCategories()
+    res.json(gamesAndCats)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
